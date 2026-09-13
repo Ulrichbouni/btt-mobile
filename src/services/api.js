bd612986bbac683635ada0d28b65375a72104c1d
unknown <Ulrichbouni>
2026-09-13 20:04:55 +0100

    fix(mobile): API_URL avec /api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants';
 
-const baseURL = Constants.expoConfig?.extra?.apiUrl || 'https://btt-backend-sgas.onrender.com';
+const baseURL = Constants.expoConfig?.extra?.apiUrl || 'https://btt-backend-sgas.onrender.com/api';
 
 const api = axios.create({ baseURL });
 
